8ede501d5d00050b2a20c76d4090539bc8ec5ff1
Jaydeep Ahuja <jaah@microsoft.com>
2020-10-20 17:16:29 -0700

    Update file header license

--- BizTalk Server/Swift/Source/ErrorResources/AssemblyInfo.cs
+++ BizTalk Server/Swift/Source/ErrorResources/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 using System.Runtime.InteropServices;
 using System.Reflection;
 using System.Runtime.CompilerServices;
